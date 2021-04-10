b757afbfa7595c9f6d9f36a7c26eec8c03d9d26e
Berin <berin2@pdx.edu>
2021-04-09 20:40:00 -0700

    Testing Jenkins file pipeline on AWS.

--- backend/auth/src/main/java/com/pixelcatsoftware/auth/AuthApplication.java
+++ backend/auth/src/main/java/com/pixelcatsoftware/auth/AuthApplication.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableWebSecurity
 @EnableJpaRepositories
 @SpringBootApplication
-@EnableDiscoveryClient
 public class AuthApplication {
 
 	/**Jenkins test 4 hook should be posting now.!!*/

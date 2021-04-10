d63f5d36d4856af99b20930ed4a192d943cc6965
Berin <berin2@pdx.edu>
2021-04-09 18:13:08 -0700

    second test

--- backend/auth/src/main/java/com/pixelcatsoftware/auth/AuthApplication.java
+++ backend/auth/src/main/java/com/pixelcatsoftware/auth/AuthApplication.java
@@ -12,7 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableDiscoveryClient
 public class AuthApplication {
 
-	/**Jenkins test.*/
+	/**Jenkins test 2.*/
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
 	}

0b8a5bcfc3599b99db1035e16460508784adb0a6
Berin <berin2@pdx.edu>
2021-04-10 13:00:26 -0700

    Testing pwd.

--- backend/auth/src/main/java/com/pixelcatsoftware/auth/AuthApplication.java
+++ backend/auth/src/main/java/com/pixelcatsoftware/auth/AuthApplication.java
@@ -2,7 +2,6 @@ package com.pixelcatsoftware.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 

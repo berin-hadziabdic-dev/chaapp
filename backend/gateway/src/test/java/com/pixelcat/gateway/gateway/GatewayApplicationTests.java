12123153c66927bd8b815f7f740e0101b83bcd2f
Berin <berin2@pdx.edu>
2021-04-11 15:55:19 -0700

    .

--- backend/gateway/src/test/java/com/pixelcat/gateway/gateway/GatewayApplicationTests.java
+++ backend/gateway/src/test/java/com/pixelcat/gateway/gateway/GatewayApplicationTests.java
@@ -3,10 +3,10 @@ package com.pixelcat.gateway.gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class GatewayApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 

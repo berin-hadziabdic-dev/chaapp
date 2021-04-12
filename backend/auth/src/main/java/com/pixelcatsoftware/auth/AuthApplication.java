011bff00295ba01fa9e37396b77676f021a64e5f
Berin <berin2@pdx.edu>
2021-04-12 16:28:36 -0700

    Altered build steps.

--- backend/auth/src/main/java/com/pixelcatsoftware/auth/AuthApplication.java
+++ backend/auth/src/main/java/com/pixelcatsoftware/auth/AuthApplication.java
@@ -2,12 +2,14 @@ package com.pixelcatsoftware.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableWebSecurity
 @EnableJpaRepositories
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthApplication {
 
 	/**Jenkins test 4 hook should be posting now.!!*/
